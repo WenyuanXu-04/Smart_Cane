566ec9c905db08cb6e1740320e31db751d109ee9
WenyuanXu-04 <2442838976@qq.com>
2024-10-01 15:08:49 +0800

    新建工程

---
+++ include/main.hpp
@@ -0,0 +1,6 @@
+#include <Arduino.h>
+#include "freertos/task.h"
+#include "freertos/FreeRTOS.h"
+#include "freertos/FreeRTOSConfig.h"
+
+void samrt_cane_app_main();
---
+++ src/main.cpp
@@ -0,0 +1,12 @@
+#include "main.hpp"
+
+
+void setup() 
+{
+
+}
+
+void loop() 
+{
+  vTaskDelay(10 / portTICK_PERIOD_MS);
+}
